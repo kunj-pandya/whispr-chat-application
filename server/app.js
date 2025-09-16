588df17ad18f2e49dfe82726813fb9bb50b7b20c
kunj-pandya <kunjpandya998@gmail.com>
2025-09-16 18:54:04 +0530

    Setup Express server with middlewares and environment configuration

---
+++ server/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import { config } from "dotenv";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+
+const app = express();
+
+config({
+    path: "./config/config.env"
+})
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+}));
+
+app.use(cookieParser);
+app.use(express.json);
+app.use(express.urlencoded({ extended: true }));
+app.use(
+    fileUpload({
+        useTempFiles: true,
+        tempFileDir: "./temp",
+    })
+);
+
+export default app;
+
