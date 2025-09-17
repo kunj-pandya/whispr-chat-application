cfe64ca2dcb10a669591bae9f122c68be1933616
kunj-pandya <kunjpandya998@gmail.com>
2025-09-17 20:35:51 +0530

    add isAuthenticated middleware in /sign-out,/me & /update-profile route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { signin, signout, signup, getUser, updateProfile } from "../controllers/user.controller.js";
+import { isAuthenticated } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/sign-up", signup);
 router.post("/sign-in", signin);
-router.get("/sign-out", signout);
-router.get("/me", getUser);
-router.put("/update-profile", updateProfile);
+router.get("/sign-out", isAuthenticated, signout);
+router.get("/me", isAuthenticated, getUser);
+router.put("/update-profile", isAuthenticated, updateProfile);
 
 
 export default router;
