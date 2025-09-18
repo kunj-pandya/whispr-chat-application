eba1aa8665d40a4e058e01961889056bbde16ed8
kunj-pandya <kunjpandya998@gmail.com>
2025-09-18 09:23:41 +0530

    add getMessages controller to fetch conversation between users

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.middleware.js";
 import { User } from "../models/user.model.js";
+import { Message } from "../models/message.model.js";
 
 export const getAllUsers = catchAsyncError(async (req, res, next) => {
     const user = req.user;
@@ -13,7 +14,30 @@ export const getAllUsers = catchAsyncError(async (req, res, next) => {
 });
 
 export const getMessages = catchAsyncError(async (req, res, next) => {
+    const receiverId = req.params.id;
+    const myId = req.user._id;
 
+    const receiver = await User.findById(receiverId);
+
+    if (!receiver) {
+        return res.status(400).json({
+            success: false,
+            message: "Reciver ID invalid."
+        });
+    }
+
+    const messages = await Message.find({
+        $or: [
+            { senderId: myId, receiverId: receiverId },
+            { senderId: receiverId, receiverId: myId },
+        ],
+    }).sort({ createdAt: 1 });
+
+
+    res.status(200).json({
+        success: true,
+        messages
+    })
 });
 
 export const sendMessage = catchAsyncError(async (req, res, next) => {
