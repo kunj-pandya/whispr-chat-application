020328093b13994092215957dbd8244e016a4780
kunj-pandya <kunjpandya998@gmail.com>
2025-09-18 20:00:41 +0530

    create authSlice with getUser async thunk and online users state

---
+++ client/src/store/slices/authSlice.js
@@ -0,0 +1,46 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { axiosInstance } from "../../lib/axios";
+import { connectSocket } from "../../lib/socket";
+
+
+export const getUser = createAsyncThunk("user/me", async (_, thunkAPI) => {
+    try {
+        const res = await axiosInstance.get("/user/me");
+        connectSocket(res.data.user);
+        return res.data.user;
+    } catch (error) {
+        console.log("Error fetching user:", error);
+        return thunkAPI.rejectWithValue(
+            error.response?.data || "Failed to fetch user"
+        );
+    }
+});
+
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState: {
+        authUser: null,
+        isSigningUp: false,
+        isLogginIn: false,
+        isUpdatingProfile: false,
+        isCheckingAuth: true,
+        onlineUsers: [],
+    }, reducers: {
+        setOnlineUsers(state, action) {
+            state.onlineUsers = action.payload;
+        },
+    }, extraReducers: (builder) => {
+        builder
+            .addCase(getUser.fulfilled, (state, action) => {
+                state.authUser = action.payload;
+                state.isCheckingAuth = false;
+            }).addCase(getUser.rejected, (state) => {
+                state.authUser = null; state.isCheckingAuth = false;
+            });
+    },
+});
+
+export const { setOnlineUsers } = authSlice.actions;
+
+export default authSlice.reducer;
