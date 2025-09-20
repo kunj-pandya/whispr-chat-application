4c33497df359a57dc5c53f0f693e5b409fb2a632
kunj-pandya <kunjpandya998@gmail.com>
2025-09-20 16:33:38 +0530

    add routes for home, /register, /login & /profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,61 @@
-import './App.css'
+import { useDispatch, useSelector } from 'react-redux';
+import './App.css';
+import { Loader } from "lucide-react";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from 'react';
+import { getUser, setOnlineUsers } from './store/slices/authSlice';
+import { connectSocket, disconnectSocket } from './lib/socket';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import { ToastContainer } from "react-toastify";
+
 
 function App() {
 
+  const { authUser, isCheckingAuth } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, [getUser]);
+
+  useEffect(() => {
+    if (authUser) {
+      const socket = connectSocket(authUser._id);
+
+      socket.on("getOnlineUsers", (users) => {
+        dispatch(setOnlineUsers(users));
+      });
+
+      return () => disconnectSocket();
+    }
+  }, [authUser]);
+
+  if (isCheckingAuth & !authUser) {
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <Loader className='size-10 animate-spin' />
+      </div>
+    );
+  }
+
   return (
     <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+          <Route path='/register' element={!authUser ? <Register /> : <Navigate to={"/"} />} />x
+          <Route path='/login' element={!authUser ? <Login /> : <Navigate to={"/"} />} />x
+          <Route path='/profile' element={authUser ? <Profile /> : <Navigate to={"/login"} />} />
+        </Routes>
+        <ToastContainer />
+      </Router>
     </>
   )
 }
