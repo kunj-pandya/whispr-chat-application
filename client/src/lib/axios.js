74e51151fad8d234257997033ee721237758ed96
kunj-pandya <kunjpandya998@gmail.com>
2025-10-10 12:35:42 +0530

    fixed typo in baseUrl

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:400/api/v1" : "/",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api/v1" : "/",
     withCredentials: true,
 });
