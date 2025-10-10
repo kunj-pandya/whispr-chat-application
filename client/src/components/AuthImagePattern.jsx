5c210681a2d8cf23753e38fe2a447527925d29e1
kunj-pandya <kunjpandya998@gmail.com>
2025-10-10 16:32:50 +0530

    add grid on sign in page

--- client/src/components/AuthImagePattern.jsx
+++ client/src/components/AuthImagePattern.jsx
@@ -1,5 +1,24 @@
-const AuthImagePattern = () => {
-    return <></>;
+const AuthImagePattern = ({ title, subtitle }) => {
+    return <>
+        <div className="hidden lg:flex items-center justify-center p-12">
+            <div className="max-w-md text-center">
+
+                {/* Grid Pattren */}
+                <div className="grid grid-cols-3 gap-3 mb-8">
+                    {[...Array(9)].map((_, i) => {
+                        return (
+                            <div
+                                key={i}
+                                className={`aspect-square rounded-2xl bg-gray-700/30 ${i % 2 === 0 ? "animate-pulse" : ""}`}></div>
+                        )
+                    })}
+                </div>
+
+                <h2 className="text-2xl font-bold text-black mb-4">{title}</h2>
+                <p className="text-gary-700">{subtitle}</p>
+            </div>
+        </div>
+    </>;
 };
 
 export default AuthImagePattern;
