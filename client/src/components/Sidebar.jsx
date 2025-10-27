2c9a4bf691f7cd5c840dfdfcafa23a8634c32734
kunj-pandya <kunjpandya998@gmail.com>
2025-10-27 10:49:17 +0530

    add user list and info in slidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -7,15 +7,19 @@ import { Users } from "lucide-react";
 const Sidebar = () => {
     const [showOnlineOnly, setShowOnlineOnly] = useState(false);
     const { users, selectedUser, isUsersLoading } = useSelector((state) => state.chat);
-    const { onlineUsers } = useSelector((state) => state.auth);
+    const { onlineUsers, authUser } = useSelector((state) => state.auth);
 
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(getUsers());
-    }, [dispatch]);
+        if (authUser) {
+            dispatch(getUsers());
+        }
+    }, [dispatch, authUser]);
 
-    const filteredUsers = showOnlineOnly ? users?.filter((u) => onlineUsers.includes(u._id)) : users;
+    const filteredUsers = users
+        ?.filter((user) => user._id !== authUser?._id)
+        ?.filter((user) => (showOnlineOnly ? onlineUsers.includes(user._id) : true));
 
     if (isUsersLoading) return <SidebarSkeleton />;
 
@@ -44,11 +48,60 @@ const Sidebar = () => {
                         Show Online Only
                     </label>
                     <span className="text-xs text-gray-500">
-                        ({onlineUsers?.length ? onlineUsers.length - 1 : 0}online)
+                        ({onlineUsers?.length || 0} online)
                     </span>
                 </div>
             </div>
 
+            {/* -------------------- USERS LIST----------------  */}
+
+            <div className="overflow-y-auto w-full py-3">
+
+                {
+                    filteredUsers.length > 0 && filteredUsers.map(user => (
+                        <button
+                            key={user._id}
+                            onClick={() => dispatch(setSelectedUser(user))}
+                            className={`w-full p-3 flex items-center gap-3 transition-colors rounded-md ${selectedUser?._id === user._id ? "bg-gray-200 ring-gray-200" : "hover:bg-gray-200"
+                                }`}
+                        >
+
+                            {/* ---------- AVATAR --------- */}
+                            <div className="relative mx-auto lg:mx-0">
+                                <img
+                                    src={user?.avatar?.url || "/avatar-holder.avif"}
+                                    alt={"/avatar-holder.avif"}
+                                    className="w-12 h-12 object-cover rounded-full"
+                                />
+                                {
+                                    onlineUsers.includes(user._id) && (
+                                        <span className="absolute bottom-0 right-0 w-3 h-3 bg-green-50 rounded-full ring-2 ring-white" />)
+                                }
+                            </div>
+                            {/* --------- USER INFO ------------ */}
+                            <div className="hidden lg:block text-left min-w-0">
+                                <div className="font-medium text-gray-800 truncate">
+                                    {user.fullname}
+                                </div>
+                                <div className="text-sm text-gray-500">
+                                    {onlineUsers.includes(user._id) ? "Online" : "Offline"}
+                                </div>
+                            </div>
+
+                        </button>
+                    ))
+                }
+
+
+                {
+                    filteredUsers?.length === 0 && (
+                        <div className="text-center text-gray-500 py-4">
+                            No Online Users
+                        </div>
+                    )
+                }
+
+            </div>
         </aside>
     );
 };
