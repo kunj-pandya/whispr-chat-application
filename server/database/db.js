1a3b2c8ff1d977962e8ff4f33ba6d1a956e363d8
kunj-pandya <kunjpandya998@gmail.com>
2025-10-29 19:31:07 +0530

    fixed typo for database

--- server/database/db.js
+++ server/database/db.js
@@ -4,7 +4,7 @@ export const dbConnection = () => {
     mongoose.connect(process.env.MONGO_URI, {
         dbName: "MERN_WHISPR_CHAT_APP"
     }).then(() => {
-        console.log("Connected to Databse.");
+        console.log("Connected to Database.");
     }).catch((error) => {
         console.log(`Error connecting to database : ${error.message || error}`);
     });
